b1f0b70c786b5899b64b378cecd4b46056e2a18a
cloudcome <cloudcome@163.com>
2014-12-16 21:47:05 +0800

    translate

---
+++ index.js
@@ -0,0 +1,14 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2014-12-16 21:46
+ */
+
+'use strict';
+
+//有道翻译API申请成功
+//API key：1787962561
+//keyfrom：f2ec-org
+//创建时间：2014-12-16
+//网站名称：f2ec-org
+//网站地址：http://f2ec.org
